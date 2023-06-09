b5508e39c808d8664cf0a150a94308752c7ffed3
Mike Haller <mike.haller@bosch.com>
2023-06-09 14:40:00 +0000

    WIP: QEMU
    Signed-off-by: GitHub <noreply@github.com>

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Build examples using Docker BuildX
 
-docker buildx create --name mybuilder
-docker buildx use mybuilder
-docker buildx inspect --bootstrap
-docker buildx build --platform linux/amd64,linux/arm64 --tag ghcr.io/eclipse-leda/leda-example-seat-adjuster:latest --progress plain - < Dockerfile.seat-adjuster
+# docker buildx create --use
+# docker buildx inspect --bootstrap
+#docker buildx build --platform linux/amd64,linux/arm64 --tag ghcr.io/eclipse-leda/leda-example-seat-adjuster:latest --progress plain - < Dockerfile.seat-adjuster
+docker build --tag leda/seat-adjuster --progress plain - < Dockerfile.seat-adjuster
