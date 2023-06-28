fb64131dfd2a502fba45b6663a7a69ebf2a70e8d
Mike Haller <mike.haller@bosch.com>
2023-06-28 14:50:42 +0000

    SIM7600 Demo Files from Waveshare
    Signed-off-by: GitHub <noreply@github.com>

---
+++ raspberry-pi/sim7600_4G_hat_init/build-arm64.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# /********************************************************************************
+# * Copyright (c) 2023 Contributors to the Eclipse Foundation
+# *
+# * See the NOTICE file(s) distributed with this work for additional
+# * information regarding copyright ownership.
+# *
+# * This program and the accompanying materials are made available under the
+# * terms of the Apache License 2.0 which is available at
+# * https://www.apache.org/licenses/LICENSE-2.0
+# *
+# * SPDX-License-Identifier: Apache-2.0
+# ********************************************************************************/
+#
+# Build examples using Docker BuildX
+#
+# Setup:
+# - sudo apt-get install -y qemu-user-static
+# - sudo apt-get install -y binfmt-support
+# - docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+# - docker buildx rm ledabuilder
+# - docker buildx create --name ledabuilder --use
+
+docker buildx build \
+    --builder ledabuilder \
+    --platform "linux/arm64" \
+    --load \
+    --tag ghcr.io/eclipse-leda/leda-example-applications/leda-example-sim7600x:latest \
+    --progress plain \
+    .
