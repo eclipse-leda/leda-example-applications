2326c107c755d372767561b00043e1dd807f4b8a
Guenther Meyer <g.meyer@signum-media.de>
2023-10-17 10:17:12 +0200

    Node-RED: add simple databroker query and VSS example
    Signed-off-by: Guenther Meyer <g.meyer@signum-media.de>

--- node-red/settings.js
+++ node-red/settings.js
@@ -4,29 +4,19 @@
 
 module.exports = {
     editorTheme: {
+        functionExternalModules: true,
         theme: "midnight-red",
-
         palette: {
             /** The following property can be used to order the categories in the editor
              * palette. If a node's category is not in the list, the category will get
              * added to the end of the palette.
              * If not set, the following default order is used:
              */
-            //categories: ['subflows', 'common', 'function', 'network', 'sequence', 'parser', 'storage'],
+            categories: ['SDV', 'subflows', 'common', 'function', 'network', 'sequence', 'parser', 'storage'],
         },
-
-        functionExternalModules: true,
-
         projects: {
-            /** To enable the Projects feature, set this value to true */
-            enabled: false,
+            enabled: true,
             workflow: {
-                /** Set the default projects workflow mode.
-                 *  - manual - you must manually commit changes
-                 *  - auto - changes are automatically committed
-                 * This can be overridden per-user from the 'Git config'
-                 * section of 'User Settings' within the editor
-                 */
                 mode: "manual"
             }
         }
